188d9d841205056df8f5e6471bf3c662c8fc4432
mukul013 <17018512+mukul013@users.noreply.github.com>
2023-06-08 00:02:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,24 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://downloads.magicanoz.com/1:/desktop-1920x1080%20copy.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
+
 def fetch_poster(movie_id):
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US')
     data = response.json()
@@ -11,7 +29,7 @@ def fetch_poster(movie_id):
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:11]
 
     recommended_movies = []
     recommended_movie_poster = []
@@ -25,16 +43,38 @@ movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-st.title('Movie Recommender System')
-
+new_title = '<p style="font-weight:bold; font-family:sans-serif; color:white; font-size: 42px; text-align: center">Movie Recommender System</p>'
+st.markdown(new_title, unsafe_allow_html=True)
 
 selected_movie_name = st.selectbox(
-    'How would you like to be contacted?',
+    '',
     movies['title'].values)
 
+st.text(" ")
+st.text(" ")
+
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+     margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    background-color: #E50914;
+    font-size: 25px;
+    
+}
+</style>""", unsafe_allow_html=True)
+
+
 if st.button('Recommend'):
+    st.text(" ")
+    st.text(" ")
     names,posters = recommend(selected_movie_name)
-    col1, col2, col3 ,col4,col5= st.columns(5)
+    col1, col2, col3 ,col4,col5= st.columns(5 , gap="medium")
 
     with col1:
         st.text(names[0])
@@ -55,3 +95,24 @@ if st.button('Recommend'):
     with col5:
         st.text(names[4])
         st.image(posters[4])
+
+    with col1:
+        st.text(names[5])
+        st.image(posters[5])
+
+    with col2:
+        st.text(names[6])
+        st.image(posters[6])
+
+    with col3:
+        st.text(names[7])
+        st.image(posters[7])
+
+    with col4:
+        st.text(names[8])
+        st.image(posters[8])
+
+    with col5:
+        st.text(names[9])
+        st.image(posters[9])
+
